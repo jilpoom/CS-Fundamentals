e54c48cfeabd9a172f39fb25d1062dd69ed76ea8
Palladium <wjddyd94@naver.com>
2024-06-19 01:14:28 +0900

    test_show_endian

--- computer-system/ch02/show_byte.c
+++ computer-system/ch02/show_byte.c
@@ -7,11 +7,12 @@ void show_int(int x);
 void show_float(float x);
 void show_pointer(void *x);
 void test_show_bytes(int val);
-
+void test_show_pointer(int hex);
 
 int main(void) 
 {
     test_show_bytes(12345);
+    test_show_pointer(0x87654321);
 }
 
 void test_show_bytes(int val) 
@@ -22,6 +23,14 @@ void test_show_bytes(int val)
     show_int(ival);
     show_float(fval);
     show_pointer(pval);
+}
+
+void test_show_pointer(int hex)
+{
+    byte_pointer valp = (byte_pointer) &hex;
+    show_bytes(valp, 1);
+    show_bytes(valp, 2);
+    show_bytes(valp, 3);
 }
 
 void show_bytes(byte_pointer start, int len) 
@@ -44,7 +53,7 @@ void show_float(float x)
     show_bytes((byte_pointer) &x, sizeof(float));
 }
 
-void show_pointer(void *x)
+void show_pointer(void *x) // void *: 어떤 타입의 주소라도 보관 가능하다.
 {
     show_bytes((byte_pointer) &x, sizeof(void *));
 }
